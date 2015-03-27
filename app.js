059a9ff18416257c25e0121bc4bf403974036cbf
Daniel Treasure <dbtreasure@gmail.com>
2015-03-27 15:14:35 -0600

    final touches

--- app.js
+++ app.js
@@ -1,20 +1,3 @@
-function jqUpdateSize(){
-    // Get the dimensions of the viewport
-    var width = $(window).width();
-    var height = $(window).height();
-
-  	if(width < 642){
-  		$(".icon-bar").show();
-  	} else{
-  		$(".icon-bar").hide();
-  	}
-};
-$(document).ready(jqUpdateSize);    // When the page first loads
-$(window).resize(jqUpdateSize);
-
-
-
-
 $(document).ready(function(){
 	$(".hidden").hide();
 	$(".panel").hover(function(){
